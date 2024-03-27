85342a750e5daafa3151d75575418578c8991b51
tr0jan172 <tin.lucic172@gmail.com>
2024-03-27 17:02:40 +0100

    Skeleton of dns enumeration

--- modules/dns_enumeration.py
+++ modules/dns_enumeration.py
@@ -1,1 +1,17 @@
 import os
+
+def amass_scan(domain):
+    '''Run an amass scan and save into a sqlite file'''
+    pass
+
+def dns_recon(domain):
+    pass
+
+def dns_enum():
+    pass
+
+def main():
+    '''Main function for doing dns recon'''
+    current_dir = os.getcwd()
+    os.mkdir(f'{current_dir}/dns')
+#os.system("dnsrecon -d google.com)
