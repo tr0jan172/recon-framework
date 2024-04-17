fc164712fde1392c704790dbc5a2ca13ba0f4b70
tr0jan172 <tin.lucic172@gmail.com>
2024-04-18 00:20:20 +0200

    Redone the skeleton code

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import os
+import threading
 
-def amass_scan(domain):
-    '''Run an amass scan and save into a sqlite file'''
-    pass
+HOST = input("What is the target? ")
 
 def dns_recon(domain):
-    pass
+    '''This function will perform DNS related tools'''
+    #os.system(f'amass enum -d {domain} -dir dns/amass_scan')
+    #os.system(f'dnsenum {domain} > dns/dnsenum_output')
+    os.system(f'subfinder -d {domain} -all -o dns/subfinder_output')
 
-def dns_enum():
-    pass
+def probe():
+    url_list = []
+    with open("dns/subfinder_output") as f:
+        for line in f:
+            url_list.append(line.rstrip())
+    os.system(f'httpx -l dns/subfinder_output -rl 5 -fc 404 -sc -location -server -o dns/httpx_output')
+
+
+
+
+
 
 def main():
-    '''Main function for doing dns recon'''
-    current_dir = os.getcwd()
+    current_dir = os.getcwd() # Get current working directory
+
+    # Setup directories for the framework
     os.mkdir(f'{current_dir}/dns')
+    os.mkdir(f'{current_dir}/network')
+    os.mkdir(f'{current_dir}/spidering')
+    dns_recon(HOST)
+    probe()
 #os.system("dnsrecon -d google.com)
+
+if __name__ == '__main__':
+    main()
